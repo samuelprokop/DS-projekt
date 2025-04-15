ce150fcf5324640609235d9ef3a47b8dcad00a60
Samuel Prokop <prokopsamuel24@gmail.com>
2025-04-15 20:58:35 +0200

    vercel fix

--- backend/server.js
+++ backend/server.js
@@ -37,3 +37,5 @@ app.use('/api/query', queryRouter);
 app.get('/', (req, res) => res.send("API Working"));
 
 app.listen(port, () => console.log(`Server running on port: ${port}`));
+
+module.exports = app;
